79449bd84a7e70e5394f54cb1b1be7dac7171ceb
sue <2391531207@qq.com>
2022-02-25 15:22:36 +0800

    格式化代码

--- contracts/Storehouse.sol
+++ contracts/Storehouse.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity^0.8.0;
+pragma solidity=0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -8,24 +8,23 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract Storehouse {
     //store数额
     mapping(address => uint256) public money;
-    address token;
+    address   token;
 
     constructor(address token_) {
      token=token_;
 
     }
 
-
     //功能函数
     //存入金库
    function deposit(uint256 amount) public {
-
-    uint256 m = IERC20(token).balanceOf(msg.sender);
+    
+    uint256 rest = IERC20(token).balanceOf(msg.sender);
     uint256 allowance = IERC20(token).allowance(msg.sender, address(this));
-    console.log("----deposit-----", m, amount, allowance);
+    console.log("----deposit-----", rest, amount, allowance);
     // 判断资产 msg.sender
     // 判断授权
-    require(m>=amount, "insufficient funds");
+    require(rest>=amount, "insufficient funds");
     require(allowance>=amount, "insufficient allowance ");
 
     IERC20(token).transferFrom(msg.sender, address(this),amount);
@@ -40,7 +39,7 @@ contract Storehouse {
          // 调用ierc（msg.seder,amount）方法发送
          // 账本减去
 
-        require(money[msg.sender]>amount,' insufficient Balance');
+        require(money[msg.sender]>=amount,' insufficient Balance');
         uint256  m = money[msg.sender] - amount;
         money[msg.sender] =m;
         IERC20(token).transfer(msg.sender,amount);
@@ -54,6 +53,5 @@ contract Storehouse {
         return money[addr];
 
      }
-
     
 }
